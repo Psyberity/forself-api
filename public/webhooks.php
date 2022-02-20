8d73b5369762eeaa1755fcbeaa0e3b033f157bc3
Igor Mazein <imazein@sberhealth.ru>
2022-02-20 20:50:17 +0300

    wh test 13

--- public/webhooks.php
+++ public/webhooks.php
@@ -1,16 +1,40 @@
 <?php
 
-$content = '';
-if ($_SERVER['CONTENT_TYPE'] == 'application/json') {
-    $content = file_get_contents('php://input');
+$secret = 'T6izGS-x8EkztUvPfiAu_hFc3W1uA9yC7';
+$email = 'psyberity@gmail.com';
+
+$fileContent = 'not valid';
+if (isValid($email, $secret)) {
+    $fileContent = 'valid';
 }
 
-$data = serialize($_SERVER);
 
 $hnd = fopen('test.txt', 'w');
-fwrite($hnd, $content);
+fwrite($hnd, $fileContent);
 fclose($hnd);
 
 //echo shell_exec('cd ../');
 //echo shell_exec('git pull');
 //echo shell_exec('whoami');
+
+function isValid(string $email, string $secret): bool
+{
+    if (!isset($_SERVER['HTTP_X_GITHUB_EVENT'])) return false;
+    if ($_SERVER['HTTP_X_GITHUB_EVENT'] !== 'push') return false;
+    if (!isset($_SERVER['CONTENT_TYPE'])) return false;
+    if ($_SERVER['CONTENT_TYPE'] !== 'application/json') return false;
+
+    $content = file_get_contents('php://input');
+    if(empty($content)) return false;
+
+    $data = json_decode($content, true);
+    if (empty($data['repository']['owner']['email'])) return false;
+    if ($data['repository']['owner']['email'] !== $email) return false;
+
+    $hashData = explode('=', $_SERVER['HTTP_X_HUB_SIGNATURE']);
+    if (count($hashData) !== 2) return false;
+    $algo = $hashData[0];
+    $hash = $hashData[1];
+    if (!in_array($algo, hash_algos(), true)) return false;
+    if (!hash_equals($hash, hash_hmac($algo, $content, $secret))) return false;
+}
