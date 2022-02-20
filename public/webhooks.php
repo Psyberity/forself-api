dab590f7e0fb46acddce614da07b7fab8190230b
Igor Mazein <imazein@sberhealth.ru>
2022-02-20 19:36:38 +0300

    webhooks test 4

--- public/webhooks.php
+++ public/webhooks.php
@@ -1,12 +1,12 @@
 <?php
 
-//$data = serialize($_POST) . '
-//' . serialize($_GET);
+$data = serialize($_POST) . '
+' . serialize($_GET);
 
-//$hnd = fopen('test.txt', 'w');
-//fwrite($hnd, $data);
-//fclose($hnd);
+$hnd = fopen('test.txt', 'w');
+fwrite($hnd, $data);
+fclose($hnd);
 
 //echo shell_exec('cd ../');
 //echo shell_exec('git pull');
-echo shell_exec('whoami');
+//echo shell_exec('whoami');
