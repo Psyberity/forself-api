5e4eec2bdc4f2a796da47e0d006eef153fe5cf00
Igor Mazein <imazein@sberhealth.ru>
2022-02-21 13:00:01 +0300

    wh composer test 15

--- public/whtest.php
+++ public/whtest.php
@@ -4,6 +4,7 @@ print_r(shell_exec('cd ../ && git pull'));
 print '<br>--<br>';
 //print_r(shell_exec('cd ../ && ls && composer install'));
 //print '<br>--<br>';
-print_r(system('php /usr/local/bin/composer install -d /var/www/api.forself.ru/'));
+print_r(system('cd ../ && php /usr/local/bin/composer/composer.phar install'));
+print_r(system('cd ../ && php /usr/local/bin/composer/composer.phar install -d /var/www/api.forself.ru/'));
 print '<br>--<br>';
 print mt_rand(1, 999);
