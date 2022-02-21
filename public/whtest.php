90d438e6d9cd9dd9efef446d23e0161d89bc369e
Igor Mazein <imazein@sberhealth.ru>
2022-02-21 12:15:44 +0300

    wh composer test 8

--- public/whtest.php
+++ public/whtest.php
@@ -1,6 +1,10 @@
 <?php
 
+print_r(shell_exec('ls'));
+print '<br>--<br>';
 print_r(shell_exec('cd ../'));
+print '<br>--<br>';
+print_r(shell_exec('ls'));
 print '<br>--<br>';
 print_r(shell_exec('git pull'));
 print '<br>--<br>';
