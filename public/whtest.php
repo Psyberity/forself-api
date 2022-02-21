e40edff32383074c4d09216db2eaf359065c1e33
Igor Mazein <imazein@sberhealth.ru>
2022-02-21 12:06:18 +0300

    wh composer test 5

--- public/whtest.php
+++ public/whtest.php
@@ -4,4 +4,6 @@ print_r(shell_exec('cd ../'));
 print '<br>--<br>';
 print_r(shell_exec('git pull'));
 print '<br>--<br>';
-print_r(shell_exec('php composer.phar install'));
+print_r(shell_exec('php composer install'));
+print '<br>--<br>';
+print mt_rand(1, 999);
